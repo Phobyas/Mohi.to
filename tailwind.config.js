6e5afbcbad551ff70274265430c92b2d0064d895
Phobyas <162043716+Phobyas@users.noreply.github.com>
2025-09-25 16:30:56 +0200

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.liquid",
+    "./sections/**/*.liquid", 
+    "./snippets/**/*.liquid",
+    "./layout/**/*.liquid"
+  ]
+}
